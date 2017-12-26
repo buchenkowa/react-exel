d5f3c67f528ab8b3ab704a3352c24abd5fd03b62
buchenkowa <buchenkowa@gmail.com>
2017-12-26 21:02:03 +0500

    README.md

---
+++ src/Table.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Table() {
+  return(
+    <div>
+      <h1>Excel Table</h1>
+      <section>Table body</section>
+    </div>
+  )
+}
+
+export default Table
---
+++ src/app.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { render } from 'react-dom'
+import Table from './Table'
+
+render(<Table />, document.getElementById('container'));
